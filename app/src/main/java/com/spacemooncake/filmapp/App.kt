87e2e1ef33d1113be99215c779cfd4360b70b01a
moonCake <klamina259@gmail.com>
2022-08-08 12:37:49 +0300

    clean code

--- app/src/main/java/com/spacemooncake/filmapp/App.kt
+++ app/src/main/java/com/spacemooncake/filmapp/App.kt
@@ -4,14 +4,12 @@ import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class App {
-    class App : Application() {
-        override fun onCreate() {
-            super.onCreate()
-            startKoin {
-                androidContext(this@App)
-                modules(appModule)
-            }
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(appModule)
         }
     }
 }
